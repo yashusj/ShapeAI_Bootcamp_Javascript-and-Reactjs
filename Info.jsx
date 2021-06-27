babab7637f572192a4887385f0ac11f59726548c
yashusj <49529397+yashusj@users.noreply.github.com>
2021-06-27 12:40:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>It was nice bootcamp tankyou</p>
+      </div>
+  );
+}
+export default Info;
